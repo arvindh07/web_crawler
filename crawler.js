50e47eebb8820d005cc2f4d1e0c5765e59dcac75
arvindh07 <arvindhganesh11@gmail.com>
2024-05-26 16:04:12 +0530

    strip protocol logic in normalize url

--- crawler.js
+++ crawler.js
@@ -1,5 +1,9 @@
 const normalizeUrl = (url) => {
-    return url;
+    // strip protocol
+    const urlObj = new URL(url);
+    console.log("host:path", urlObj.hostname, urlObj.pathname);
+    const newUrl = `${urlObj.hostname}${urlObj.pathname}`;
+    return newUrl;
 }
 
 module.exports = {
--- crawler.test.js
+++ crawler.test.js
@@ -2,9 +2,15 @@ const {normalizeUrl} = require("./crawler");
 // const {describe, it} = require("jest");
 
 describe("normalize url testing", () => {
-    it("should normalize a valid url", () => {
+    it("should normalize a url", () => {
         const inputUrl = "http://wagslane.com";
-        const expected = "wagslane.com";
+        const expected = "wagslane.com/";
+        const actual = normalizeUrl(inputUrl);
+        expect(actual).toBe(expected);
+    })
+    it("should strip protocol from url", () => {
+        const inputUrl = "http://wagslane.com";
+        const expected = "wagslane.com/";
         const actual = normalizeUrl(inputUrl);
         expect(actual).toBe(expected);
     })
