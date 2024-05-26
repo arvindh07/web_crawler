580594cd86e1c5e000d06009fcbfc56f7e9588b3
arvindh07 <arvindhganesh11@gmail.com>
2024-05-26 16:26:28 +0530

    added jsdom and completed geturls from html body function

--- crawler.js
+++ crawler.js
@@ -1,11 +1,30 @@
+const {JSDOM} = require("jsdom");
+
 const normalizeUrl = (url) => {
     // strip protocol
     const urlObj = new URL(url);
-    console.log("host:path", urlObj.hostname, urlObj.pathname);
     const newUrl = `${urlObj.hostname}${urlObj.pathname}`;
     return newUrl;
 }
 
+const getLinksFromHtml = (htmlbody, baseUrl) => {
+    const urls = [];
+    const dom = new JSDOM(htmlbody);
+    const anchorTags = dom.window.document.getElementsByTagName("a");
+    for (const tag of anchorTags) {
+        const link = tag.href;
+        if(link.charAt(0) === "/"){
+            // relative url -> base url + path
+            urls.push(`${baseUrl}${link}`);
+        } else{
+            // absolute url
+            urls.push(link);
+        }
+    }
+    return urls;
+}
+
 module.exports = {
-    normalizeUrl
+    normalizeUrl,
+    getLinksFromHtml
 }
--- crawler.test.js
+++ crawler.test.js
@@ -1,17 +1,63 @@
-const {normalizeUrl} = require("./crawler");
-// const {describe, it} = require("jest");
+const { normalizeUrl, getLinksFromHtml } = require("./crawler");
 
 describe("normalize url testing", () => {
     it("should normalize a url", () => {
         const inputUrl = "http://wagslane.com";
         const expected = "wagslane.com/";
         const actual = normalizeUrl(inputUrl);
-        expect(actual).toBe(expected);
+        expect(actual).toEqual(expected);
     })
     it("should strip protocol from url", () => {
         const inputUrl = "http://wagslane.com";
         const expected = "wagslane.com/";
         const actual = normalizeUrl(inputUrl);
-        expect(actual).toBe(expected);
+        expect(actual).toEqual(expected);
+    })
+})
+
+describe("get links from html body", () => {
+    it("get links", () => {
+        const htmlBody = `
+            <html>
+                <body>
+                    <a href="http://wagslane.com/path">Link to home</a>
+                </body>
+            </html>
+        `;
+        const baseUrl = "http://wagslane.com";
+        const expected = ["http://wagslane.com/path"];
+        const actual = getLinksFromHtml(htmlBody, baseUrl);
+        expect(actual).toEqual(expected);
+    })
+    it("get relative url", () => {
+        const htmlBody = `
+            <html>
+                <body>
+                    <button>
+                        <a href="/about">Link to About</a>
+                    </button>
+                </body>
+            </html>
+        `;
+        const baseUrl = "http://wagslane.com";
+        const expected = ["http://wagslane.com/about"];
+        const actual = getLinksFromHtml(htmlBody, baseUrl);
+        expect(actual).toEqual(expected);
+    })
+    it("get multiple urls", () => {
+        const htmlBody = `
+            <html>
+                <body>
+                    <a href="http://wagslane.com/path">Link to home</a>
+                    <button>
+                        <a href="/about">Link to About</a>
+                    </button>
+                </body>
+            </html>
+        `;
+        const baseUrl = "http://wagslane.com";
+        const expected = ["http://wagslane.com/path", "http://wagslane.com/about"];
+        const actual = getLinksFromHtml(htmlBody, baseUrl);
+        expect(actual).toEqual(expected);
     })
 })
