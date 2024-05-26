bf590c2d397e3977ea21839caca7e6c6ecf3f023
arvindh07 <arvindhganesh11@gmail.com>
2024-05-26 16:34:34 +0530

    covered corner case for cmd line args

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+function main(){
+    if(process.argv.length < 3){
+        console.log("no url provided");
+        console.log("So exiting...");
+        process.exit(1);
+    } else if(process.argv.length > 3){
+        console.log("cant process multiple urls");
+        process.exit(1);
+    }
+    console.log(process.argv);
+}
+
+main();
